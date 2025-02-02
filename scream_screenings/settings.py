03c1e0cc768fb1bb28f5aae8c512edf6e1ebdf70
Thom Hunkin <thom.hunkin@gmail.com>
2025-02-02 15:14:39 +0000

    Update files for heroku deployment

--- scream_screenings/settings.py
+++ scream_screenings/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+q#ec=&(%o2*3$%7osh(l%6na34_-r&40@o982)tkx5s0xrkg)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
