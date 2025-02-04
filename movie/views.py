72b3a9275b0493f1c80c6119b608d356f2afe042
Thom Hunkin <thom.hunkin@gmail.com>
2025-02-04 21:54:50 +0000

    debugging heroku

--- movie/views.py
+++ movie/views.py
@@ -10,6 +10,10 @@ def movie_list(request):
 def movie_detail(request, movie_slug):
     # Fetch the movie by its ID or return a 404 if not found
     movie = get_object_or_404(Movie, slug=movie_slug)
+
+    # Debugging: print movie attributes to console/log
+    print(movie.title, movie.slug, movie.description)
+
     return render(request, 'movie/movie-detail.html', {'movie': movie})
 
 # movies = Movie.objects.all().order_by('screening_datetime')[:6]
