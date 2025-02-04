30a5ac61bd252d39a758e87a7fb20e81d02ebeb1
Thom Hunkin <thom.hunkin@gmail.com>
2025-02-04 21:45:55 +0000

    Testing heroku deployment

--- movie/views.py
+++ movie/views.py
@@ -4,10 +4,12 @@ from .models import Movie
 # Create your views here.
 def movie_list(request):
     # Fetch the first six movies in the Movie model
-    movies = Movie.objects.all().order_by('screening_datetime')[:6]
+    movies = Movie.objects.all()
     return render(request, 'movie/movie-list.html', {'movies': movies})
 
 def movie_detail(request, movie_slug):
     # Fetch the movie by its ID or return a 404 if not found
     movie = get_object_or_404(Movie, slug=movie_slug)
     return render(request, 'movie/movie-detail.html', {'movie': movie})
+
+# movies = Movie.objects.all().order_by('screening_datetime')[:6]
