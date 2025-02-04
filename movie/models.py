792ed846d2b6ef4f05d6587202f4b19dfa9fa3fb
Thom Hunkin <thom.hunkin@gmail.com>
2025-02-04 12:09:10 +0000

    Update Movie model with duration field

--- movie/models.py
+++ movie/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from datetime import timedelta
 
 # Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     release_date = models.DateField()
     description = models.TextField()
+    duration = models.DurationField(default=timedelta(minutes=90))
     director = models.CharField(max_length=255)
     poster = models.ImageField(upload_to='movie_posters/', null=True, blank=True)
 
